013660c9a165cc09bf86434f823901e2535b9f95
geauxtigergo <109235362+geauxtigergo@users.noreply.github.com>
2023-11-23 00:55:42 -0600

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -5,9 +5,9 @@ import json
 def predict_mpg(config):
     ##loading the model from the saved file
     try:
-        pkl_filename = "hello.py"
-        with open(pkl_filename, 'rb') as f_in:
-             model = pickle.load(f_in)
+       # pkl_filename = "hello.py"
+       # with open(pkl_filename, 'rb') as f_in:
+       #      model = pickle.load(f_in)
        
         if type(config) == dict:
             df = pd.DataFrame(config)
