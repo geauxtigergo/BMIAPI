da4cf99e0264fab255914a62e4a00776415de570
geauxtigergo <109235362+geauxtigergo@users.noreply.github.com>
2023-11-23 00:54:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,3 @@ class GetPredictionOutput(Resource):
 
 api.add_resource(Test,'/')
 api.add_resource(GetPredictionOutput,'/getPredictionOutput')
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
